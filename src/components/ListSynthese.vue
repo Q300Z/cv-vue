d06f5003b0ebe7a92baf622c252487d5da706b1d
Q300Z <Q300Zhomas@Gmail.com>
2024-03-21 13:55:13 +0100

    update listsynthese

--- src/components/ListSynthese.vue
+++ src/components/ListSynthese.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
-import {useAppStore} from "../stores/app";
-import {ref} from "vue";
-import {ItemSynthese} from "../models/itemSynthese";
+import { useAppStore } from "../stores/app";
+import { ref } from "vue";
+import { ItemSynthese } from "../models/itemSynthese";
 
 const appStore = useAppStore();
 const itemSynthese = ref(appStore.getItemSynthese);
@@ -12,15 +12,10 @@ function setItemSynthese(item: ItemSynthese) {
 </script>
 
 <template>
-  <v-list density="compact" max-height="90vh" nav>
+  <v-list density="compact" nav>
     <v-card v-for="(group, index) in itemSynthese" :key="index" class="mb-2" variant="text">
       <div class="font-weight-bold">{{ group.title }}</div>
-      <v-list-item
-        v-for="(child, i) in group.children"
-        :key="i"
-        :title="child.title"
-        @click="setItemSynthese(child)"
-      />
+      <v-list-item v-for="(child, i) in group.children" :key="i" :title="child.title" @click="setItemSynthese(child)" />
       <v-divider></v-divider>
     </v-card>
   </v-list>
